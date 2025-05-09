550630eef607be34c13be890be3f4e36db52569f
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2025-05-09 13:43:55 -0400

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
   // update the JSX being returned!
   return (
     <div id="home">
-      <h1 style= { divStyle }>Your { name } is a Web Developer from Your { city }</h1>
+      <h1 style= { divStyle }>{ name } is a Web Developer from { city }</h1>
     </div>
   );
 }
